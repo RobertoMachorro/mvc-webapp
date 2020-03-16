ecac032f4bc955d463edd0f8675400d7ceac17bf
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2020-03-15 20:51:40 -0400

    Added http service support using core for application.

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 'use strict'
 
-exports.init = function (port) {
-	console.log('PORT', port)
+const debug = require('debug')('webapp:service')
+const http = require('http')
+
+const core = require('./core')
+
+exports.start = function (applicationRoot, listenPort) {
+	debug('listenPort', listenPort)
+
+	const app = core.create(applicationRoot, listenPort)
+	const server = http.createServer(app)
+
+	server.on('error', onError)
+	server.on('listening', onListening)
+
+	server.listen(listenPort)
+}
+
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error
+	}
+
+	const bind = typeof port === 'string'
+		? 'Pipe ' + port
+		: 'Port ' + port
+
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges')
+			process.exit(1)
+			break
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use')
+			process.exit(1)
+			break
+		default:
+			throw error
+	}
+}
+
+function onListening() {
+	debug('online')
 }
